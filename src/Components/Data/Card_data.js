1d2cee8488bcd3a44d59dc9b6ced296ec283bd05
Shabbir Anjum <mahrshabbir768.com>
2024-02-01 20:34:53 +0500

    fdf

---
+++ src/Components/Data/Card_data.js
@@ -0,0 +1,57 @@
+const card_data = [
+    {
+      title: "Regular Beef Burger",
+      about: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      price: "$40.3",
+      img:'/src/Components/img/beverage-1.png'
+    },
+    {
+      title: "Regular Beef Burger",
+      about: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      price: "$40.3",
+      img:'/src/Components/img/beverage-2.png'
+    },
+    {
+      title: "Regular Beef Burger",
+      about: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      price: "$40.3",
+      img:'/src/Components/img/beverage-3.png'
+    },
+    {
+      title: "Regular Beef Burger",
+      about: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      price: "$40.3",
+      img:'/src/Components/img/beverage-4.png'
+    },
+    {
+      title: "Regular Beef Burger",
+      about: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      price: "$40.3",
+      img:'/src/Components/img/beverage-5.png'
+    },
+    {
+      title: "Regular Beef Burger",
+      about: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      price: "$40.3",
+      img:'/src/Components/img/beverage-1.png'
+    },
+    {
+      title: "Regular Beef Burger",
+      about: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      price: "$40.3",
+      img:'/src/Components/img/beverage-1.png'
+    },
+    {
+      title: "Regular Beef Burger",
+      about: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      price: "$40.3",
+      img:'/src/Components/img/beverage-1.png'
+    },
+    {
+      title: "Regular Beef Burger",
+      about: "Lorem ipsum dolor sit amet consectetur adipisicing elit",
+      price: "$40.3",
+      img:'/src/Components/img/beverage-1.png'
+    },
+  ];
+  export default card_data;
--- src/Components/Menu/Menu.jsx
+++ src/Components/Menu/Menu.jsx
@@ -1,27 +1,50 @@
 import React from "react";
-
+import card_data from '../Data/Card_data'
 export default function Menu() {
   return (
     <>
-      <div className="container mt-8">
-        <div className="text-center mx-auto">
+      <div className="container mt-20">
+        <div className="text-center mx-auto w-1/2">
           <h2 className="title">Our Best Menu</h2>
-          <div className="separator"></div>
+          <div className="seperator mx-auto"></div>
           <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore,
-            natus fugiat dolores provident enim vitae earum labore eligendi
-            quasi, omnis iusto! Id cumque, harum molestias ducimus incidunt esse
-            aut! Esse!
+            Tempore, natus fugiat dolores provident enim vitae earum labore
+            eligendi quasi, omnis iusto! Id cumque, harum molestias ducimus
+            incidunt esse aut! Esse!
           </p>
-         <div className="tab">
+          <div className="tab">
             <ul className="flex mt-8 justify-center gap-4 flex-wrap">
-                <li className="btn2 flex-1">All</li>
-                <li className="btn2 flex-1">Food</li>
-                <li className="btn2 flex-1">Snack</li>
-                <li className="btn2 flex-1">Beverage</li>
+              <li className="btn2 flex-1">All</li>
+              <li className="btn2 flex-1">Food</li>
+              <li className="btn2 flex-1">Snack</li>
+              <li className="btn2 flex-1">Beverage</li>
+            
             </ul>
-         </div>
+          </div>
         </div>
+        <div className="grid grid-cols-1 mt-8 md:grid-cols-2 lg:grid-cols-4 gap-8 lg:gap-12  ">
+        {card_data.map((card,index)=>(
+      
+          <div key={index}>
+            <div className="hover:bg-secondaryColor hover:opacity-70 p-16 rounded-3xl mb-4 md:h-42 lg:p-4 bg-primaryColorLight">
+              <img
+                src={card.img}
+                alt="Error"
+                className="w-20 mx-auto"
+              />
+            </div>
+            <div>
+              <h4 className="2xl uppercase font-Roboto">{card.title}</h4>
+              <p>
+                {card.about}
+              </p>
+              <div className="mt-2 font-Roboto text-fuchsia-300">{card.price}</div>
+            </div>
+          </div>
+          
+        ))}
+        </div>
+       
       </div>
     </>
   );
